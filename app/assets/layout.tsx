013502e48c4e3d87730af4e8208b99361b61efc8
Muhammad Kholil Aziz Ashari <emkahaaa@gmail.com>
2024-08-19 10:09:39 +0700

    fix: remove not needed

--- app/assets/layout.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const layout = ({ children }: { children: React.ReactNode }) => {
-    return (
-        <div className='p-8 overflow-hidden'>{children}</div>
-    )
-}
-
-export default layout
